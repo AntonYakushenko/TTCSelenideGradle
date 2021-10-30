b622f2f40112c7d9b69944a62e42d3f07d4d42a1
antonyakusenko <mnbv0@rambler.ru>
2021-10-30 21:49:23 +0400

    add telegram notifications

--- src/test/java/test/FiveRequest.java
+++ src/test/java/test/FiveRequest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class FiveRequest extends TestBase {
     Pages pages = new Pages();
+
     @ParameterizedTest
     @ValueSource(strings = {"карта мап купить", "карта мап купить спб","карточка мап купить","карточка мап купить спб"})
     void firstTest(String text) throws InterruptedException {
